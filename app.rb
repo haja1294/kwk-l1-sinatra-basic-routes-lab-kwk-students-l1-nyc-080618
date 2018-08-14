0ca2bc7db5573015cb4799eede985d4e2d74a185
Haja Trawally <hajatrawally6@gmail.com>
2018-08-14 17:16:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+get'/name'do
+  "Hello my name is Haja!"
+end
+  get'/hometown'do 
+    "My hometown is New York City"
+  end
+  
+    get'/favorite-song'do
+      "My favorite song is Halo"
+    end
 end
